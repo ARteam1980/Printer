6e9e592da407c7f5397283dd7e70433e9bdd68b4
ARteam <revatema08@gmail.com>
2024-08-01 10:00:05 +0100

    Main_code

--- txt.py
+++ txt.py
@@ -1,3 +1,4 @@
+import csv
 import subprocess
 import time
 import sys
@@ -43,34 +44,6 @@ n_count = 0
 county = 3
 bruh = 0
 coconut = 0
-root = tk.Tk()
-def show_message():
-    label["text"] = entry.get()
-root.geometry("320x320")
-placeholder = 'Input your preset loc.'
-def erase(event=None):
-    if e.get() == placeholder:
-        e.delete(0,'end')
-def add(event=None):
-    if e.get() == '':
-        e.insert(0,placeholder)
-def erase(event=None):
-    if f.get() == placeholder:
-        f.delete(0,'end')
-def add(event=None):
-    if f.get() == '':
-        f.insert(0,placeholder)
-
-e = tk.Entry(root)
-e.pack(padx=10,pady=10)
-f = tk.Entry(root)
-f.pack(padx=10,pady=10)
-add()
-e.bind('<FocusIn>',erase)
-e.bind('<FocusOut>',add)
-f.bind('<FocusIn>',erase)
-f.bind('<FocusOut>',add)
-root.mainloop()
 # u = "agawa;aragwa;wawa;bauwa" 
 def bigger(file, x_size, y_size, use_baw):
     with Image.open(file) as fil:
@@ -107,15 +80,11 @@ def Make_It_Readable(x):
 # # if False:   
 # N = 0
 def Make_inf(x):
-    xz = np.loadtxt(
-        x,
-        dtype = str,
-        )
-    y = str(xz)
-    return(re.split(r""";|
-""", y))
-def finnaly_make_it(x, y):
-    z = x[y]
+    with open("printer.csv", encoding='utf-8') as r_file:
+        file_reader = csv.DictReader(r_file, delimiter = ",")
+        print(type(file_reader))
+        return(file_reader)
+
 # def make_QR(x):
 #     img = qrcode.make(x)
 #     type(img)
@@ -143,66 +112,67 @@ po = 1080 * lo
 io = 1980 * lo
 bigger(op[0]["destination_preset"], po, io, int(op[0]["Use_company_colors"])).save('another.png')
 o = int(input("Uhhh"))
-while count != round(len(Make_inf(str(op[0]['destination_csv']))) / len(op[0]['Objects']) - 1):
-    y = len(list(op[0]['Objects'])) * count
-    za = Make_It_Readable(str(Make_inf(op[0]['destination_csv'])[county]))
-    if za_count != len(list(op[0]['Objects'])):
-        county += 1
-        if op[0]['Objects'][za_count]['Type'] == 'text':
-            list2.append(za)
-            list2.append('text')
-        if op[0]['Objects'][za_count]['Type'] == 'QR':
-            list2.append(za)
-            list2.append('QR')
-        if op[0]['Objects'][za_count]['Type'] == 'DATA':
-            list2.append(za)
-            list2.append('DATA')
-        za_count += 1
-    else:
-        for i in range(len(list2)-1):
-            if n_count == 0:
-                n_count += 1
-            n_count += 2
-            if list2[n_count] == 'text':
-                print(list4)
-                list3.append(list2[omg])
-                if len(list3) == len(list2) / 2:
-                    p = make_text(bigger(op[0]['destination_preset'], 1980 * int(op[0]['Scale_multiplyer']), 1080 * int(op[0]['Scale_multiplyer']), op[0]['Use_company_colors']), str(list3), 50, 200, 200)
-                    list3 = []
-            if list2[n_count] == 'QR':
-                list4.append(list2[omg])
-                if len(list4) == len(list2) / 2:
-                    print(list4)
-                    if p == 0:
-                        pa = make_QR(bigger(op[0]['destination_preset'], 1980 * int(op[0]['Scale_multiplyer']), 1080 * int(op[0]['Scale_multiplyer']), op[0]['Use_company_colors']), str(list4), op[0]['Link'], 1300, 200)
-                    else:
-                        pa = make_QR(p, str(list4), op[0]['Link'], 1300, 200)
-                    True_count += 1
-                    list4 = []
-            Tr_cntr += 1
-            aaaa += 1
-            count = 0
-            if n_count == len(list2)-1:
-                n_count = 0
-            if o == 1:
-                if p != 0 and pa == 0:
-                    p.save(op[0]["dest_output"] + "printer"  + str(True_count) + ".png")
-                    p = 0
-                    True_count += 1
-                if pa != 0 and p == 0:
-                    pa.save(op[0]["dest_output"] + "printer"  + str(True_count) + ".png")
-                    pa = 0
-                    True_count += 1
-                if p != 0 and pa != 0:
-                    pa().save(op[0]["dest_output"] + "printer"  + str(True_count) + ".png")
-                    p = 0
-                    pa = 0
-                    True_count += 1
-            omg += 2
-            if aaaa >= len(list2) - 2:
-                aaaa = 0
-            if omg == len(list2):
-                omg = 0
-        za_count = 0
-        list2 = []
-    count += 1
+while
+# while count != round(len(Make_inf(str(op[0]['destination_csv']))) / len(op[0]['Objects']) - 1):
+#     y = len(list(op[0]['Objects'])) * count
+#     za = Make_It_Readable(str(Make_inf(op[0]['destination_csv'])[county]))
+#     if za_count != len(list(op[0]['Objects'])):
+#         county += 1
+#         if op[0]['Objects'][za_count]['Type'] == 'text':
+#             list2.append(za)
+#             list2.append('text')
+#         if op[0]['Objects'][za_count]['Type'] == 'QR':
+#             list2.append(za)
+#             list2.append('QR')
+#         if op[0]['Objects'][za_count]['Type'] == 'DATA':
+#             list2.append(za)
+#             list2.append('DATA')
+#         za_count += 1
+#     else:
+#         for i in range(len(list2)-1):
+#             if n_count == 0:
+#                 n_count += 1
+#             n_count += 2
+#             if list2[n_count] == 'text':
+#                 print(list4)
+#                 list3.append(list2[omg])
+#                 if len(list3) == len(list2) / 2:
+#                     p = make_text(bigger(op[0]['destination_preset'], 1980 * int(op[0]['Scale_multiplyer']), 1080 * int(op[0]['Scale_multiplyer']), op[0]['Use_company_colors']), str(list3), 50, 200, 200)
+#                     list3 = []
+#             if list2[n_count] == 'QR':
+#                 list4.append(list2[omg])
+#                 if len(list4) == len(list2) / 2:
+#                     print(list4)
+#                     if p == 0:
+#                         pa = make_QR(bigger(op[0]['destination_preset'], 1980 * int(op[0]['Scale_multiplyer']), 1080 * int(op[0]['Scale_multiplyer']), op[0]['Use_company_colors']), str(list4), op[0]['Link'], 1300, 200)
+#                     else:
+#                         pa = make_QR(p, str(list4), op[0]['Link'], 1300, 200)
+#                     True_count += 1
+#                     list4 = []
+#             Tr_cntr += 1
+#             aaaa += 1
+#             count = 0
+#             if n_count == len(list2)-1:
+#                 n_count = 0
+#             if o == 1:
+#                 if p != 0 and pa == 0:
+#                     p.save(op[0]["dest_output"] + "printer"  + str(True_count) + ".png")
+#                     p = 0
+#                     True_count += 1
+#                 if pa != 0 and p == 0:
+#                     pa.save(op[0]["dest_output"] + "printer"  + str(True_count) + ".png")
+#                     pa = 0
+#                     True_count += 1
+#                 if p != 0 and pa != 0:
+#                     pa.save(op[0]["dest_output"] + "printer"  + str(True_count) + ".png")
+#                     p = 0
+#                     pa = 0
+#                     True_count += 1
+#             omg += 2
+#             if aaaa >= len(list2) - 2:
+#                 aaaa = 0
+#             if omg == len(list2):
+#                 omg = 0
+#         za_count = 0
+#         list2 = []
+#     count += 1
