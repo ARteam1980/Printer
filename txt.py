9106ca0270b5fdc8b3503c0ebd3504516a4fa493
ARteam <revatema08@gmail.com>
2024-08-02 09:42:02 +0300

    Main_code

--- txt.py
+++ txt.py
@@ -1,3 +1,4 @@
+# Библиотеки
 import csv
 import subprocess
 import time
@@ -44,7 +45,7 @@ n_count = 0
 county = 3
 bruh = 0
 coconut = 0
-# u = "agawa;aragwa;wawa;bauwa" 
+# Создание изображения с определённым размером и использованием чб
 def bigger(file, x_size, y_size, use_baw):
     with Image.open(file) as fil:
         fil.load()
@@ -54,44 +55,13 @@ def bigger(file, x_size, y_size, use_baw):
         fil = fil.convert('L').point(fn, mode='1')
     new = fil.resize((x_size,y_size))
     return(new)
-#     if usage == 1:
-#         ar = '#bb0000'
-#     else:
-#         ar = '#000000'
-#     dwg = open(file, "w")
-#     dwg.write(f"""<?xml version="1.0" encoding="utf-8" ?>
-# <svg baseProfile="basic" height="{x_scale}" version="1.1" width="{y_scale}" xmlns="http://www.w3.org/2000/svg"
-#     xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink">
-#     <defs />
-#     <rect fill="white" height="{x_scale}" stroke="{ar}" width="{y_scale}" x="0" y="0" />
-# </svg>""")
-#     dwg.close()
-#     drawing = svg2rlg(file)
-#     renderPM.drawToFile(drawing, "file.png", fmt="PNG")
-#     z = open('file.png')
-#     return(z)
-def Make_It_Readable(x):
-    # print(x.replace("""[""", """""").replace("""]""", """""").replace("""'""", """""").replace(""";""", """-"""))
-    return(x.replace("""[""", """""").replace("""]""", """""").replace("""'""", """""").replace(""" """, """
-""").replace("""'""", """""").replace(""",""", """ """).replace("""
-""", """"""))
-# # Make_It_Readable(y)
-# # Make_It_Readable(u)
-# # if False:   
-# N = 0
+# Создаёт словарь из csv файла
 def Make_inf(x):
     with open("printer.csv", encoding='utf-8') as r_file:
         file_reader = csv.DictReader(r_file, delimiter = ",")
         print(type(file_reader))
         return(file_reader)
-
-# def make_QR(x):
-#     img = qrcode.make(x)
-#     type(img)
-#     img.save("QR.png")
-#     with Image.open('QR.png') as QR:
-#         QR.load()
-#     QR.show()
+# Создаёт QR код из исходного изображения
 def make_QR(File, Info, Link, loc_x, loc_y):
     img = qrcode.make(str(Info) + " visit us at: " + Link)
     img.save("QR.png")
@@ -100,10 +70,11 @@ def make_QR(File, Info, Link, loc_x, loc_y):
         QR.load()
     og.paste(QR, (loc_x, loc_y))
     return(og)
+# Добавляет текст на изображение
 def make_text(file, What_to_print, font_size, x, y):
     Font = ImageFont.truetype("C:/Users/Reva.A/The_installation/ok.ttf", size=font_size)
     drawText = ImageDraw.Draw(file)
-    drawText.text((x,y), Make_It_Readable(str(What_to_print)), fill=('#ae0817'), font=Font, spacing=100)
+    drawText.text((x,y), What_to_print, fill=('#ae0817'), font=Font, spacing=100)
     file.save("Preset.png")
     x = Image.open("Preset.png")
     return(x)
@@ -112,7 +83,8 @@ po = 1080 * lo
 io = 1980 * lo
 bigger(op[0]["destination_preset"], po, io, int(op[0]["Use_company_colors"])).save('another.png')
 o = int(input("Uhhh"))
-while
+while True:
+    pass
 # while count != round(len(Make_inf(str(op[0]['destination_csv']))) / len(op[0]['Objects']) - 1):
 #     y = len(list(op[0]['Objects'])) * count
 #     za = Make_It_Readable(str(Make_inf(op[0]['destination_csv'])[county]))
